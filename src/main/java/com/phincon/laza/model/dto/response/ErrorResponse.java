d3e3ec48aae915cbc0181e84042a6900da7e09f7
aldiramdan <aldi.ramdan96@gmail.com>
2023-09-05 11:43:43 +0700

    Update error and pagination response

--- src/main/java/com/phincon/laza/model/dto/response/ErrorResponse.java
+++ src/main/java/com/phincon/laza/model/dto/response/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.phincon.laza.model.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +11,7 @@ import lombok.NoArgsConstructor;
 public class ErrorResponse {
     private Integer statusCode;
     private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private Object subError;
 }
