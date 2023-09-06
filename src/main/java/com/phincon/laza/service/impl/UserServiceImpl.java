a4defa2f6360d9688a3566e557ca4aeb4d05b977
aldiramdan <aldi.ramdan96@gmail.com>
2023-09-06 09:58:44 +0700

    Add feature user change password

--- src/main/java/com/phincon/laza/service/impl/UserServiceImpl.java
+++ src/main/java/com/phincon/laza/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.phincon.laza.service.impl;
 
+import com.phincon.laza.model.dto.request.ChangePasswordRequest;
 import com.phincon.laza.model.dto.request.UserRequest;
 import com.phincon.laza.model.entity.User;
 import com.phincon.laza.repository.UserRepository;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,9 +19,9 @@ import java.util.Optional;
 @Slf4j
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-
     private final UserRepository userRepository;
     private final UserValidator userValidator;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public Page<User> getAll(Pageable pageable) {
@@ -28,14 +30,14 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User getByUsername(String username) throws Exception {
+    public User getByUsername(String username) {
         Optional<User> findUser = userRepository.findByUsername(username);
         userValidator.validateUserNotFound(findUser);
         return findUser.get();
     }
 
     @Override
-    public User update(String username, UserRequest request) throws Exception {
+    public User update(String username, UserRequest request) {
         Optional<User> findUser = userRepository.findByUsername(username);
         userValidator.validateUserNotFound(findUser);
 
@@ -48,4 +50,15 @@ public class UserServiceImpl implements UserService {
         return user;
     }
 
+    @Override
+    public void changePassword(String username, ChangePasswordRequest request) {
+        userValidator.validateUserPasswordNotMatch(request.getNewPassword(), request.getConfirmPassword());
+
+        Optional<User> findUser = userRepository.findByUsername(username);
+        userValidator.validateUserNotFound(findUser);
+        userValidator.validateInvalidOldPassword(request.getOldPassword(), findUser.get().getPassword());
+
+        findUser.get().setPassword(passwordEncoder.encode(request.getConfirmPassword()));
+        userRepository.save(findUser.get());
+    }
 }
