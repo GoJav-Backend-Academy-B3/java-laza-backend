1684b57f222e33541b62af2ff519d4a4ed47a2ad
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-06 15:06:29 +0700

    add new method to accept multipartfile

--- src/main/java/com/phincon/laza/service/CloudinaryImageService.java
+++ src/main/java/com/phincon/laza/service/CloudinaryImageService.java
@@ -1,7 +1,11 @@
 package com.phincon.laza.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.phincon.laza.model.dto.other.CloudinaryUploadResult;
 
 public interface CloudinaryImageService {
-  CloudinaryUploadResult upload(byte[] bytes, String folder, String fileId) throws Exception ;
+    CloudinaryUploadResult upload(byte[] bytes, String folder, String fileId) throws Exception;
+
+    CloudinaryUploadResult upload(MultipartFile file, String folder) throws Exception;
 }
