e03dc4daf407e214e1e454457226f2b9b60ff60c
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-06 13:00:31 +0700

    retab

--- src/main/java/com/phincon/laza/model/dto/other/CloudinaryUploadResult.java
+++ src/main/java/com/phincon/laza/model/dto/other/CloudinaryUploadResult.java
@@ -3,21 +3,22 @@ package com.phincon.laza.model.dto.other;
 import java.util.Map;
 
 public record CloudinaryUploadResult(
-    String publicId,
-    int width,
-    int height,
-    String format,
-    int bytes,
-    String secureUrl) {
-  public static CloudinaryUploadResult fromMap(Map map) {
-    return new CloudinaryUploadResult(
-        (String) map.get("public_id"),
-        (int) map.get("width"),
-        (int) map.get("height"),
-        (String) map.get("format"),
-        (int) map.get("bytes"),
-        (String) map.get("secure_url"));
-  }
+        String publicId,
+        int width,
+        int height,
+        String format,
+        int bytes,
+        String secureUrl) {
+    public static CloudinaryUploadResult fromMap(Map map) {
+        return new CloudinaryUploadResult(
+                (String) map.get("public_id"),
+                (int) map.get("width"),
+                (int) map.get("height"),
+                (String) map.get("format"),
+                (int) map.get("bytes"),
+                (String) map.get("secure_url"));
+    }
+
     public static CloudinaryUploadResult empty() {
         return new CloudinaryUploadResult(null, 0, 0, null, 0, null);
     }
