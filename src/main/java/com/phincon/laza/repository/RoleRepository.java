420d4d9b8bc0358459fb84f3a44e6e6e46efdbfb
aldiramdan <aldi.ramdan96@gmail.com>
2023-09-06 08:46:17 +0700

    Add service role

---
+++ src/main/java/com/phincon/laza/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.phincon.laza.repository;
+
+import com.phincon.laza.model.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
