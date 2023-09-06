8c4cec82ea1b4e028d6d817a9b07681368eb4b49
aldiramdan <aldi.ramdan96@gmail.com>
2023-09-06 12:30:38 +0700

    Updated exception

--- src/main/java/com/phincon/laza/exception/CustomExceptionHandler.java
+++ src/main/java/com/phincon/laza/exception/CustomExceptionHandler.java
@@ -5,12 +5,14 @@ import com.phincon.laza.exception.custom.ConflictException;
 import com.phincon.laza.exception.custom.NotFoundException;
 import com.phincon.laza.exception.custom.NotProcessException;
 import com.phincon.laza.model.dto.response.ErrorResponse;
+import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -70,4 +72,17 @@ public class CustomExceptionHandler {
         return ResponseEntity.status(errorResponse.getStatusCode()).body(errorResponse);
     }
 
+    @ExceptionHandler(value = MessagingException.class)
+    public ResponseEntity<ErrorResponse> handleMessagingException(MessagingException e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage(), null);
+        log.warn("MessagingException: {}", e.getMessage());
+        return ResponseEntity.internalServerError().body(errorResponse);
+    }
+
+    @ExceptionHandler(value = TemplateInputException.class)
+    public ResponseEntity<ErrorResponse> handleTemplateInputException(TemplateInputException e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage(), null);
+        log.warn("TemplateInputException: {}", e.getMessage());
+        return ResponseEntity.internalServerError().body(errorResponse);
+    }
 }
