065665d56571255f12d87a2a95537393199c02b1
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-07 11:32:43 +0700

    add create product on product service

--- src/main/java/com/phincon/laza/service/ProductsService.java
+++ src/main/java/com/phincon/laza/service/ProductsService.java
@@ -1,10 +1,12 @@
 package com.phincon.laza.service;
 
 
+import com.phincon.laza.model.dto.request.CreateProductRequest;
 import com.phincon.laza.model.entity.Product;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ProductsService {
     Product getProductById(Long id) throws Exception;
+    Product create(CreateProductRequest createProductRequest) throws Exception;
 }
