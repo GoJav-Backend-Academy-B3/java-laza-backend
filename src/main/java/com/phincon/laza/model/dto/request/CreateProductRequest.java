b0514709b44700fd69fbce2941b48e8ea4704a5a
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-07 16:26:32 +0700

    add product request

--- src/main/java/com/phincon/laza/model/dto/request/CreateProductRequest.java
+++ src/main/java/com/phincon/laza/model/dto/request/CreateProductRequest.java
@@ -1,9 +1,17 @@
 package com.phincon.laza.model.dto.request;
 
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record CreateProductRequest(
         String name,
         String description,
         Integer price,
-        Iterable<Long> sizes,
-        Long category) {
+        MultipartFile file,
+        @JsonProperty("size_ids") List<Long> sizeIds,
+        @JsonProperty("category_id") Long categoryId,
+        @JsonProperty("brand_id") Long brandId) {
 }
