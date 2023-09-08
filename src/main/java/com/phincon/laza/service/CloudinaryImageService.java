c3c8afb2074b29b9e5f86e4939d19fdd62845319
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-08 10:46:25 +0700

    add new upload overload

--- src/main/java/com/phincon/laza/service/CloudinaryImageService.java
+++ src/main/java/com/phincon/laza/service/CloudinaryImageService.java
@@ -7,5 +7,7 @@ import com.phincon.laza.model.dto.other.CloudinaryUploadResult;
 public interface CloudinaryImageService {
     CloudinaryUploadResult upload(byte[] bytes, String folder, String fileId) throws Exception;
 
+    CloudinaryUploadResult upload(MultipartFile file, String folder, String fileId) throws Exception;
+
     CloudinaryUploadResult upload(MultipartFile file, String folder) throws Exception;
 }
