15eb6222b03d8e739c32277ef555040764e50ba9
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-08 16:44:58 +0700

    asdasdasd

--- src/main/java/com/phincon/laza/controller/ProductsController.java
+++ src/main/java/com/phincon/laza/controller/ProductsController.java
@@ -25,7 +25,7 @@ public class ProductsController {
     private final ProductsService productsService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<DataResponse<Product>> getProductById(@PathVariable Long id) throws Exception {
+    public ResponseEntity<DataResponse<ProductsResponse>> getProductById(@PathVariable Long id) throws Exception {
         Product product = productsService.getProductById(id);
         ProductsResponse result = new ProductsResponse(product);
         DataResponse<ProductsResponse> dataResponse = new DataResponse<>(HttpStatus.OK.value(), "Success", result,
