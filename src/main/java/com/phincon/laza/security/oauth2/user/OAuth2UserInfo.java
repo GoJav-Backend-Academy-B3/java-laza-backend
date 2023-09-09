aae3a9e6ce8b1595d35d3e52de67f0eb76cadf00
aldiramdan <aldi.ramdan96@gmail.com>
2023-09-09 20:55:51 +0700

    Add oauth2user info

---
+++ src/main/java/com/phincon/laza/security/oauth2/user/OAuth2UserInfo.java
@@ -0,0 +1,21 @@
+package com.phincon.laza.security.oauth2.user;
+
+import java.util.Map;
+
+public abstract class OAuth2UserInfo {
+    protected Map<String, Object> attributes;
+
+    public abstract String getId();
+    public abstract String getName();
+    public abstract String getEmail();
+    public abstract String getImageUrl();
+    public abstract String getVerified();
+
+    public OAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+}
