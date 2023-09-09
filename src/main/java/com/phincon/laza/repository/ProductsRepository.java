58db973a0f5d7a870e1ad8c4e9a749671ae0ec19
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-09 13:26:18 +0700

    add find by name containing function

--- src/main/java/com/phincon/laza/repository/ProductsRepository.java
+++ src/main/java/com/phincon/laza/repository/ProductsRepository.java
@@ -1,13 +1,14 @@
 package com.phincon.laza.repository;
 
-import com.phincon.laza.model.entity.Product;
-import com.phincon.laza.model.entity.Size;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.phincon.laza.model.entity.Product;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Product, Long> {
 
+    Page<Product> findByNameContaining(String keyword, Pageable page);
 }
