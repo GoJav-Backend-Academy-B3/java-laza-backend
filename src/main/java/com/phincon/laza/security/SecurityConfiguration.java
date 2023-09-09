52ddf9d995f548864094157b1141ac0e01401a8d
aldiramdan <aldi.ramdan96@gmail.com>
2023-09-09 20:57:43 +0700

    Update security for oauth2

--- src/main/java/com/phincon/laza/security/SecurityConfiguration.java
+++ src/main/java/com/phincon/laza/security/SecurityConfiguration.java
@@ -4,13 +4,14 @@ import com.phincon.laza.model.entity.ERole;
 import com.phincon.laza.security.jwt.JwtAccessDeniedHandler;
 import com.phincon.laza.security.jwt.JwtAuthenticationEntryPoint;
 import com.phincon.laza.security.jwt.JwtAuthenticationFilter;
+import com.phincon.laza.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
+import com.phincon.laza.security.oauth2.SysOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -24,6 +25,12 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final SysOAuth2UserService sysOAuth2UserService;
+
+    @Bean
+    public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
+        return new HttpCookieOAuth2AuthorizationRequestRepository();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -38,8 +45,15 @@ public class SecurityConfiguration {
                         .requestMatchers(PATCH, adminListedRoutes).hasAuthority(ERole.ADMIN.name())
                         .requestMatchers(DELETE, adminListedRoutes).hasAuthority(ERole.ADMIN.name())
                         .anyRequest().authenticated())
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2Login(oauth -> oauth
+                        .authorizationEndpoint(endpoint -> endpoint
+                                .baseUri("/oauth2/authorize")
+                                .authorizationRequestRepository(cookieAuthorizationRequestRepository()))
+                        .redirectionEndpoint(redirect -> redirect
+                                .baseUri("/oauth2/callback/**"))
+                        .userInfoEndpoint(user -> user
+                                .userService(sysOAuth2UserService))
+                        .defaultSuccessUrl("/auth/token", true))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exception -> exception
@@ -50,7 +64,9 @@ public class SecurityConfiguration {
     }
 
     public static final String[] whiteListedRoutes = new String[]{
+            "/error",
             "/auth/**",
+            "/oauth2/**",
             "/size/**",
             "/category/**",
             "/product/**",
