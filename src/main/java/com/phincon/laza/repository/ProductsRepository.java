433e2d8379104d1f83791bd981c8e55ba2d0780c
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-11 21:22:28 +0700

    add missing imports, again???

--- src/main/java/com/phincon/laza/repository/ProductsRepository.java
+++ src/main/java/com/phincon/laza/repository/ProductsRepository.java
@@ -1,9 +1,14 @@
 package com.phincon.laza.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.phincon.laza.model.entity.Product;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Product, Long> {
