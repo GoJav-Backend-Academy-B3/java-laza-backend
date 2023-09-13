834d630f407e6afafab779be0f8dc721d64b8771
unknown <ariwiraatmaja@gmail.com>
2023-09-13 09:32:59 +0700

    create testing controller address

--- src/test/java/com/phincon/laza/controller/AddressControllerTest.java
+++ src/test/java/com/phincon/laza/controller/AddressControllerTest.java
@@ -4,34 +4,42 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.phincon.laza.model.dto.request.AddressRequest;
 import com.phincon.laza.model.dto.response.DataResponse;
 import com.phincon.laza.model.entity.Address;
+import com.phincon.laza.model.entity.User;
+import com.phincon.laza.security.userdetails.SysUserDetails;
 import com.phincon.laza.service.AddressService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
-@ActiveProfiles("test")
-@SpringJUnitConfig(AddressControllerTest.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+//@ActiveProfiles("test")
 public class AddressControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private AddressService addressService;
 
     @Autowired
@@ -39,8 +47,8 @@ public class AddressControllerTest {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Test
-    public void whenAddRequestToAddress_thenCorrectResponse() throws Exception {
+    @BeforeEach
+    public void init() {
         AddressRequest request = new AddressRequest();
         request.setProvinceName("Province");
         request.setCityName("city");
@@ -52,9 +60,29 @@ public class AddressControllerTest {
         Address address = new Address();
         address.setId(1L);
 
-        DataResponse<Address> responseData = new DataResponse<Address>(HttpStatus.CREATED.value(), "Success", address, null);
+        lenient().when(addressService.add("1", request)).thenReturn(address);
+    }
 
-        when(addressService.add(anyString(), any(AddressRequest.class))).thenReturn(address);
+    @Test
+    @WithMockUser(username = "ari", password = "asd", authorities = "USER")
+    public void whenAddRequestToAddress_thenCorrectResponse() throws Exception {
+        User user = new User();
+        user.setId("1");
+
+        AddressRequest request = new AddressRequest();
+        request.setProvinceName("Province");
+        request.setCityName("city");
+        request.setFullAddress("Jl jln");
+        request.setReceiverName("Ari");
+        request.setPhone("1234567890");
+        request.setPrimary(true);
+
+        Address address = new Address();
+        address.setId(1L);
+        address.setUser(user);
+
+        when(addressService.add(user.getId(), request)).thenReturn(address);
+        DataResponse<Address> responseData = new DataResponse<Address>(HttpStatus.CREATED.value(), "Success", address, null);
 
         mockMvc.perform(MockMvcRequestBuilders
                 .post("/address")
@@ -65,5 +93,41 @@ public class AddressControllerTest {
                 .andExpect(jsonPath("$.message").value(responseData.getMessage()))
                 .andExpect(jsonPath("$.data").exists());
 
+        verify(addressService, times(1)).add("1", request);
     }
+
+    @Test
+    @WithMockUser(username = "ari", password = "asd", authorities = "USER")
+    public void testAddAddress_Success() throws Exception {
+        // Simulasi data yang diperlukan
+        SysUserDetails user = new SysUserDetails();
+        user.setId("1");
+
+        AddressRequest request = new AddressRequest();
+        request.setProvinceName("Province");
+        request.setCityName("city");
+        request.setFullAddress("Jl jln");
+        request.setReceiverName("Ari");
+        request.setPhone("1234567890");
+        request.setPrimary(true);
+
+        Address address = new Address();
+        address.setId(1L);
+
+        when(addressService.add(user.getId(), request)).thenReturn(address);
+
+        // Eksekusi metode yang diuji
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/address")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.statusCode").value(HttpStatus.CREATED.value()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Success"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data").exists());
+
+        // Verifikasi bahwa metode addressService.add dipanggil dengan argumen yang benar
+        verify(addressService, times(1)).add(user.getId(), request);
+    }
+
 }
