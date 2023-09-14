f455688334f952f19b9257fa10cec5b5b70cf544
aldiramdan <aldi.ramdan96@gmail.com>
2023-09-14 14:56:59 +0700

    Add validate duplicate name role

--- src/main/java/com/phincon/laza/validator/RoleValidator.java
+++ src/main/java/com/phincon/laza/validator/RoleValidator.java
@@ -1,16 +1,28 @@
 package com.phincon.laza.validator;
 
+import com.phincon.laza.exception.custom.BadRequestException;
 import com.phincon.laza.exception.custom.NotFoundException;
 import com.phincon.laza.model.entity.Role;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleValidator {
     public void validateRoleNotFound(Optional<Role> findRole) {
         if (findRole.isEmpty()) {
             throw new NotFoundException("Role not found");
+        }
+    }
+
+    public void validateRoleDuplicate(List<String> listRole) {
+        if (listRole.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().anyMatch(entry -> entry.getValue() > 1)) {
+            throw new BadRequestException("Role has duplicate name");
         }
     }
 }
