971231ff288a0b848926f5b2d56b05372786ed4b
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-09-15 00:18:52 +0700

    fixing rajaongkir

--- src/main/java/com/phincon/laza/model/dto/request/ROCostRequest.java
+++ src/main/java/com/phincon/laza/model/dto/request/ROCostRequest.java
@@ -1,6 +1,8 @@
 package com.phincon.laza.model.dto.request;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -13,10 +15,15 @@ import lombok.NoArgsConstructor;
 public class ROCostRequest {
     @NotBlank(message = "origin is required")
     private String origin;
+
     @NotBlank(message = "destination is required")
     private String destination;
+
+    @Min(value = 1, message = "The minimum weight is 1")
+    @Max(value = 30000,message = "The maximum weight is 30000")
     @NotNull(message = "weight is required")
     private Integer weight;
+
     @NotBlank(message = "courier is required")
     private String courier;
 }
