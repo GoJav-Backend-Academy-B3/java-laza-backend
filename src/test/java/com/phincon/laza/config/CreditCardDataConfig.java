ddbf49cbcb543a2c604438446ae4e09bf2306aeb
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-14 21:56:05 +0700

    add dummy data for credit card

---
+++ src/test/java/com/phincon/laza/config/CreditCardDataConfig.java
@@ -0,0 +1,26 @@
+package com.phincon.laza.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import com.phincon.laza.model.entity.CreditCard;
+import com.phincon.laza.model.entity.User;
+
+
+@TestConfiguration
+public class CreditCardDataConfig {
+
+    @Bean
+    @Qualifier("cc.all")
+    public List<CreditCard> creditCardAll() {
+        return Arrays.asList(
+            new CreditCard("cc01", "2739952966392831", 2, 25, null),
+            new CreditCard("cc02", "2503234676183548", 3, 24, null),
+            new CreditCard("cc03", "3881845703890574", 10, 24, null)
+        );
+    }
+}
