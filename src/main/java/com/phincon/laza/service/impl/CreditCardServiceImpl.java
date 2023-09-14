8ed8082b3da7fad745e991569ac4478eb86a231c
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-14 21:53:34 +0700

    implement get all credit card with user id

---
+++ src/main/java/com/phincon/laza/service/impl/CreditCardServiceImpl.java
@@ -0,0 +1,23 @@
+package com.phincon.laza.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.phincon.laza.model.entity.CreditCard;
+import com.phincon.laza.repository.CreditCardRepository;
+import com.phincon.laza.service.CreditCardService;
+
+@Service
+public class CreditCardServiceImpl implements CreditCardService {
+    
+    @Autowired
+    private CreditCardRepository repository;
+
+    @Override
+    public List<CreditCard> getAll(String userId) {
+        return repository.findAllByUserId(userId);
+    }
+}   
+
