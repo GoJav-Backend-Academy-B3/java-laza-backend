bcd0adc6419154b644c63f72aae126dae52cd66a
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-14 21:55:49 +0700

    add find user by id on credit card repository

--- src/main/java/com/phincon/laza/repository/CreditCardRepository.java
+++ src/main/java/com/phincon/laza/repository/CreditCardRepository.java
@@ -1,8 +1,12 @@
 package com.phincon.laza.repository;
 
-import com.phincon.laza.model.entity.CreditCard;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.phincon.laza.model.entity.CreditCard;
 
 public interface CreditCardRepository extends JpaRepository<CreditCard, String> {
 
+    List<CreditCard> findAllByUserId(String userId);
 }
