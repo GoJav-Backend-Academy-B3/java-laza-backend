0ee4111d103d7c743cc9bc77afd269be5180b24f
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-15 13:47:32 +0700

    add credit card control

--- src/main/java/com/phincon/laza/controller/CreditCardController.java
+++ src/main/java/com/phincon/laza/controller/CreditCardController.java
@@ -7,15 +7,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.phincon.laza.model.dto.request.CreateUpdateCreditCardRequest;
 import com.phincon.laza.model.dto.response.CreditCardResponse;
 import com.phincon.laza.model.dto.response.DataResponse;
+import com.phincon.laza.model.entity.CreditCard;
 import com.phincon.laza.security.userdetails.CurrentUser;
 import com.phincon.laza.security.userdetails.SysUserDetails;
 import com.phincon.laza.service.CreditCardService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -38,5 +43,13 @@ public class CreditCardController {
         var cc = service.getById(id);
         var response = CreditCardResponse.fromEntity(cc);
         return DataResponse.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<DataResponse<CreditCardResponse>> createCreditCard(@CurrentUser SysUserDetails currentUser, @Valid @RequestBody CreateUpdateCreditCardRequest request) {
+        CreditCard cc = service.create(currentUser.getId(), request);
+        var response = CreditCardResponse.fromEntity(cc);
+
+        return DataResponse.created(response);
     }
 }
