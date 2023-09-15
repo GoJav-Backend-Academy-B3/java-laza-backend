645a0d75c55c93a47de62f3ef0a11952c91d7c5c
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-15 10:09:56 +0700

    implement get by id on credit card service

--- src/main/java/com/phincon/laza/service/impl/CreditCardServiceImpl.java
+++ src/main/java/com/phincon/laza/service/impl/CreditCardServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.phincon.laza.exception.custom.NotFoundException;
 import com.phincon.laza.model.entity.CreditCard;
 import com.phincon.laza.repository.CreditCardRepository;
 import com.phincon.laza.service.CreditCardService;
@@ -18,6 +19,13 @@ public class CreditCardServiceImpl implements CreditCardService {
     @Override
     public List<CreditCard> getAll(String userId) {
         return repository.findAllByUserId(userId);
+    }
+
+    @Override
+    public CreditCard getById(String id) {
+        return repository.findById(id).orElseThrow(() -> {
+            return new NotFoundException("Credit Card not found");
+        });
     }
 }   
 
