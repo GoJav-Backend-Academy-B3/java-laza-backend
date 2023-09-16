f0132d09c8e10df275cd3378e4b206ec198f678a
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-16 20:27:02 +0700

    create the validator for custom annotation

---
+++ src/main/java/com/phincon/laza/validator/FileContentTypeCheck.java
@@ -0,0 +1,27 @@
+package com.phincon.laza.validator;
+
+import java.util.Arrays;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class FileContentTypeCheck implements ConstraintValidator<FileContentType, MultipartFile> {
+
+    private String[] contentType;
+
+    @Override
+    public void initialize(FileContentType constraintAnnotation) {
+        this.contentType = constraintAnnotation.contentType();
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(MultipartFile value, ConstraintValidatorContext context) {
+        String fileContentType = value.getContentType();
+
+        return Arrays.asList(contentType).contains(fileContentType);
+    }
+
+}
