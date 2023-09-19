6f1c799572b2f33d39bb8b5a97a73b51e964def2
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-19 19:44:45 +0700

    remove isDeleted

--- src/main/java/com/phincon/laza/model/entity/Product.java
+++ src/main/java/com/phincon/laza/model/entity/Product.java
@@ -54,8 +54,6 @@ public class Product {
   
     private String cloudinaryPublicId;
     
-    private Boolean isDeleted = false;
-
     @ManyToOne
     @JoinColumn(name = "brand_id", nullable = false)
     @JsonManagedReference
