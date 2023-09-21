a63150fb6bd460f9521bd421ceebe5ab5413fa5a
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-21 08:55:40 +0700

    fix product reposutory

--- src/main/java/com/phincon/laza/repository/ProductsRepository.java
+++ src/main/java/com/phincon/laza/repository/ProductsRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.phincon.laza.model.entity.Product;
@@ -13,7 +14,13 @@ import com.phincon.laza.model.entity.Product;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE p.isDeleted=false")
+    Page<Product> findAll(Pageable page);
+
+    @Query("SELECT p FROM Product p WHERE p.isDeleted=false AND LOWER(p.name) LIKE LOWER(CONCAT('%', ?1, '%'))")
     Page<Product> findByNameContaining(String keyword, Pageable page);
+
     Optional<Product> findByIdAndWishlistById(Long productId, String userId);
     Page<Product> findAllByWishlistById(String userId, Pageable pageable);
 }
